3c48365fab8057c17be817ac3d837adc38628da4
Mani <manipatta747@gmail.com>
2023-06-09 06:41:46 +0530

    Making some changes to test Poll SCM

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -37,7 +37,7 @@ public class TestGreeter {
 
     @Test
   public void greetShouldIncludeGreetingMessage() {
-    String someone = "World, Thanks you making me Happy";
+    String someone = "HELLO, WELCOME TO my WORLDDDD!!!!!";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
